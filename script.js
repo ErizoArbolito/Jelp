94a70f7b0efdacb435b0e456d403c9c33bde4866
ErizoArbolito <erizoarbolito@gmail.com>
2026-03-05 18:56:08 +0000

    hgh

---
+++ script.js
@@ -0,0 +1,11 @@
+const final1 = document.getElementById("muerte1");
+
+final1.addEventListener("click", ()=> {
+    window.close();
+})
+
+const final2 = document.getElementById("muerte2");
+
+final2.addEventListener("click", ()=> {
+    window.close();
+})
