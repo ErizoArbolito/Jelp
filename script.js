4a8fd006ce2bbb62859abf38f48f0e5ae0e3fe17
ErizoArbolito <erizoarbolito@gmail.com>
2026-03-10 18:51:15 +0000

    errores

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const final1 = document.getElementById("muerte1");
-
+if(final1) {
 final1.addEventListener("click", ()=> {
     window.close();
 })
+}
 
 const final2 = document.getElementById("muerte2");
 
+if(final2) {
 final2.addEventListener("click", ()=> {
     window.close();
 })
+}
